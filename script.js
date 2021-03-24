bba54a9c7f29fd437f1946b6df88ce4b8f5749ab
Matthew Miller <matt.r.miller09@gmail.com>
2021-03-24 11:19:24 -0500

    adding js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+var trackList = document.getElementById('searchInput');
+var trackBtn = document.getElementById('searchBtn');
+
+
+
+
+function getNord() {
+
+    var apiKey = '2a54e289878ded53f01816a3fddaaa3e';
+    var url = `https://api2.postnord.com/rest/shipment/v5/trackandtrace/findByIdentifier.json?apikey=${apiKey}&id=96932007555SE&locale=en&callback=<string>`;
+
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+        // // for (var i = 0; i < data.length; i++) {
+        // //     var listItem = document.createElement('li');
+        // //     listItem.textContent = data[i]
+        // }
+    });
+
+}
 var searchBtn = document.querySelector("#searchBtn");
 var searchInput = document.querySelector("#searchInput");
 
@@ -18,3 +42,7 @@ var getUrl = function(){
 };
 
 searchBtn.addEventListener("click", getUrl);
+
+
+
+getNord();
